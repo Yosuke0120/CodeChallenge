a3ee2577beff1614c9cecc039b1944c1d71f5272
yosuke.dev <fulldev1015@gmail.com>
2021-05-27 06:18:31 +0100

    add the addUser

---
+++ frontend/src/user/components/modal/AddUser.js
@@ -0,0 +1,81 @@
+import React, { createRef, useState } from 'react';
+import { Modal, Form, Button, Col } from 'react-bootstrap';
+
+const AddUser = props => {
+  const [newUser, setNewUser] = useState();
+  const addRef = createRef();
+  const onSubmitAddUser = e => {
+    e.preventDefault();
+    const addUser = {
+      username: addRef.current.name.value,
+      email: addRef.current.email.value,
+      full_name: addRef.current.fullName.value,
+      password: addRef.current.pass.value,
+      role: addRef.current.role.value
+    }
+    setNewUser(addUser);
+    console.log('addUser', addUser);
+  }
+
+
+
+  return (
+    < Modal show={props.show} onHide={props.onHide} >
+      <Modal.Header closeButton>
+        <Modal.Title>Add New User</Modal.Title>
+      </Modal.Header>
+
+      <Form noValidate ref={addRef} onSubmit={onSubmitAddUser}>
+        <Modal.Body>
+          <Form.Row>
+            <Col>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control name="newName" placeholder="New User Name" required />
+                <div className="invalid-feedback">
+                  <div className="fieldError"> Invalid value.</div>
+                </div>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control name="newEmail" placeholder="Email"
+                  required />
+                <div className="invalid-feedback">
+                  <div className="fieldError"> Invalid value.</div>
+                </div>
+              </Form.Group>
+              <Form.Group controlId="fullName">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control name="newFullName" placeholder="Full Name"
+                  required />
+                <div className="invalid-feedback">
+                  <div className="fieldError"> Invalid value.</div>
+                </div>
+              </Form.Group>
+              <Form.Group controlId="pass">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="newPass" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="role">
+                <Form.Label>Role</Form.Label>
+                <Form.Control name="newRole" placeholder="Role"
+                  required />
+                <div className="invalid-feedback">
+                  <div className="fieldError"> Invalid value.</div>
+                </div>
+              </Form.Group>
+            </Col>
+          </Form.Row>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="primary" type="submit">
+            Save
+        </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal >
+  )
+}
+
+export default AddUser;
--- frontend/src/user/pages/Users/Users.js
+++ frontend/src/user/pages/Users/Users.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { UserApi } from '../../services/UserApi';
 import { Container, Row, Col, Table, Button } from 'react-bootstrap';
+import AddUser from '../../components/modal/AddUser';
 
 export function Users(props) {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState();
 
+  const [show, setShow] = useState(false)
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false)
 
   useEffect(() => {
     UserApi.getAllUsers()
@@ -24,7 +28,8 @@ export function Users(props) {
   }, [users]);
 
   const addNewUser = async () => {
-    newUser && await UserApi.createUser(newUser)
+    // newUser && await UserApi.createUser(newUser)
+    handleShow();
   }
 
   const renderUsers = users.map((user, idx) => {
@@ -41,26 +46,29 @@ export function Users(props) {
     )
   })
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Button onClick={addNewUser}>Add user</Button>
-          <Table>
-            <thead>
-              <tr>
-                <th>Email</th>
-                <th>Full Name</th>
-                <th>Role</th>
-                <th>Created At</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {renderUsers}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Button onClick={addNewUser}>Add user</Button>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Full Name</th>
+                  <th>Role</th>
+                  <th>Created At</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {renderUsers}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+      <AddUser show={show} onHide={handleClose} />
+    </>
   )
 }
